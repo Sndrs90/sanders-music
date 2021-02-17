55f25f06b53891b16cf378b4601c2d16135aade5
Sanders <sndrs90@gmail.com>
2021-02-17 10:25:05 +0300

    Day 2. Continue working on design of MainActivity and Playlist activity.

--- app/src/main/java/com/example/sandersmusic/MainActivity.java
+++ app/src/main/java/com/example/sandersmusic/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.sandersmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Find the View with first playlist image
+        ImageView playlist1 = (ImageView) findViewById(R.id.playlist1);
+
+        // Set a click listener on that View
+        playlist1.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the playlist1 image is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playlist1Intent = new Intent(MainActivity.this, Playlist.class);
+                startActivity(playlist1Intent);
+            }
+        });
     }
 }
